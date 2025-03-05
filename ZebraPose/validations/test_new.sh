5ebaf19631f19209a5313d225f14b2b2900cccd3
Elia Bonetto <eliabntt94@gmail.com>
2025-03-05 01:16:54 +0100

    Update test_new.sh

--- ZebraPose/validations/test_new.sh
+++ ZebraPose/validations/test_new.sh
@@ -20,86 +20,86 @@ for folder in $main_folder/*; do
     if [[ $dirname == *"s_"* ]] || [[ $dirname == *"mix"* ]]; then
 
         if [ ! -f "$output_folder/$dirname""_ap10.txt" ]; then
-              bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10".txt
+              bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_ap10_OZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10_27_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_OZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10_27_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_OZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k_OZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k_27_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_OZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k_27_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_OZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_ap10_NZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10_27_NZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_NZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10_27_NZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_NZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k_NZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k_27_NZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_NZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k_27_NZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_NZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_zoo.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/zoo_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_zoo".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/zoo_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_zoo".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_300.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/300_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_300".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/300_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_300".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_grevy_zebras.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/grevy_zebras_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_grevy_zebras".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/grevy_zebras_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_grevy_zebras".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_unsup_dom_ad_horse.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/unsup_dom_ad_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_unsup_dom_ad_horse".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/unsup_dom_ad_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_unsup_dom_ad_horse".txt
         fi
     else
 
         if [ ! -f "$output_folder/$dirname""_ap10.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10".txt
 	fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_ap10_OZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_OZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_OZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k_OZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_OZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k_OZ.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_OZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_ap10_NZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/ap10_N_Z.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_NZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/ap10_N_Z.py $(find $folder/ -iname best*) > $output_folder/$dirname"_ap10_NZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_apt36k_NZ.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/apt36k_N_Z.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_NZ".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/apt36k_N_Z.py $(find $folder/ -iname best*) > $output_folder/$dirname"_apt36k_NZ".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_300.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/300.py $(find $folder/ -iname best*) > $output_folder/$dirname"_300".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/300.py $(find $folder/ -iname best*) > $output_folder/$dirname"_300".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_grevy_zebras.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/grevy_zebras.py $(find $folder/ -iname best*) > $output_folder/$dirname"_grevy_zebras".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/grevy_zebras.py $(find $folder/ -iname best*) > $output_folder/$dirname"_grevy_zebras".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_zoo.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/zoo.py $(find $folder/ -iname best*) > $output_folder/$dirname"_zoo".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/zoo.py $(find $folder/ -iname best*) > $output_folder/$dirname"_zoo".txt
         fi
 
         if [ ! -f "$output_folder/$dirname""_unsup_dom_ad_horse.txt" ]; then
-          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/unsup_dom_ad.py $(find $folder/ -iname best*) > $output_folder/$dirname"_unsup_dom_ad_horse".txt
+          bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/unsup_dom_ad.py $(find $folder/ -iname best*) > $output_folder/$dirname"_unsup_dom_ad_horse".txt
         fi
     fi
 done
-#        bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/learn_syn_anim_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_learn_syn_an_horse".txt
-#        bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/GRADE_test/learn_syn_anim.py $(find $folder/ -iname best*) > $output_folder/$dirname"_learn_syn_an_horse".txt
+#        bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/learn_syn_anim_27.py $(find $folder/ -iname best*) > $output_folder/$dirname"_learn_syn_an_horse".txt
+#        bash ../tools/dist_test.sh ../configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/test/learn_syn_anim.py $(find $folder/ -iname best*) > $output_folder/$dirname"_learn_syn_an_horse".txt
